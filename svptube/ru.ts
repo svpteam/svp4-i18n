65764255b4c9827b5daf6d9730f17b9714fd5370
chainikdn <chainik.dn@gmail.com>
2016-07-04 17:41:03 +0300

    = minor changes in svptube

--- svptube/ru.ts
+++ svptube/ru.ts
@@ -52,143 +52,153 @@
 <context>
     <name>TubePlugin</name>
     <message>
+        <location filename="tubeplugin.cpp" line="65"/>
+        <source>Show SVPtube window</source>
+        <translation>Показать окно SVPtube</translation>
+    </message>
+    <message>
         <location filename="tubeplugin.cpp" line="67"/>
         <source>Online videos</source>
         <translation>Потоковое видео</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="68"/>
+        <location filename="tubeplugin.cpp" line="70"/>
+        <source>Show video formats window...</source>
+        <translation>Показать окно выбора формата...</translation>
+    </message>
+    <message>
+        <location filename="tubeplugin.cpp" line="74"/>
         <source>Monitor clipboard</source>
         <translation>Следить за буфером обмена</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="71"/>
+        <location filename="tubeplugin.cpp" line="77"/>
         <source>Autoplay</source>
         <translation>Автоматическое проигрывание</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="75"/>
+        <location filename="tubeplugin.cpp" line="81"/>
         <source>Preferred resolution</source>
         <translation>Предпочтительный размер видео</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="77"/>
+        <location filename="tubeplugin.cpp" line="83"/>
         <source>240p</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="78"/>
+        <location filename="tubeplugin.cpp" line="84"/>
         <source>360p</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="79"/>
+        <location filename="tubeplugin.cpp" line="85"/>
         <source>480p</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="80"/>
+        <location filename="tubeplugin.cpp" line="86"/>
         <source>720p</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="81"/>
+        <location filename="tubeplugin.cpp" line="87"/>
         <source>1080p</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="82"/>
+        <location filename="tubeplugin.cpp" line="88"/>
         <source>2K</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="83"/>
+        <location filename="tubeplugin.cpp" line="89"/>
         <source>4K</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="88"/>
+        <location filename="tubeplugin.cpp" line="94"/>
         <source>Prefer 3D formats</source>
         <translation>Предпочитать 3D форматы</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="90"/>
+        <location filename="tubeplugin.cpp" line="96"/>
         <source>Video formats filter</source>
         <translation>Фильтр форматов</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="93"/>
+        <location filename="tubeplugin.cpp" line="99"/>
         <source>Show MP4 only (more stable)</source>
         <translation>Только MP4 </translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="94"/>
+        <location filename="tubeplugin.cpp" line="100"/>
         <source>Show VP9 and MP4 (VP9 preferred)</source>
         <translation>Только VP9 и MP4</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="95"/>
+        <location filename="tubeplugin.cpp" line="101"/>
         <source>Show all formats</source>
         <translation>Показывать все форматы</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="101"/>
+        <location filename="tubeplugin.cpp" line="107"/>
         <source>Hide DASH [*] formats</source>
         <translation>Скрывать форматы DASH</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="104"/>
+        <location filename="tubeplugin.cpp" line="110"/>
         <source>Hide 3D formats</source>
         <translation>Скрывать 3D форматы</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="109"/>
+        <location filename="tubeplugin.cpp" line="115"/>
         <source>Video player</source>
         <translation>Проигрыватель видео</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="113"/>
+        <location filename="tubeplugin.cpp" line="119"/>
         <source>System default</source>
         <translation>По-умолчанию в системе</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="118"/>
+        <location filename="tubeplugin.cpp" line="124"/>
         <source>mpv player</source>
         <translation>mpv</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="121"/>
-        <location filename="tubeplugin.cpp" line="130"/>
+        <location filename="tubeplugin.cpp" line="127"/>
+        <location filename="tubeplugin.cpp" line="136"/>
         <source>Choose application...</source>
         <translation>Выбрать...</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="127"/>
+        <location filename="tubeplugin.cpp" line="133"/>
         <source>Player for 3D</source>
         <translation>Проигрыватель для 3D</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="129"/>
+        <location filename="tubeplugin.cpp" line="135"/>
         <source>Same as player for 2D</source>
         <translation>Тот же, что для 2D</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="135"/>
+        <location filename="tubeplugin.cpp" line="141"/>
         <source>User manual (online)</source>
         <translation>Справка (на сайте)</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="141"/>
+        <location filename="tubeplugin.cpp" line="149"/>
         <source>SVPtube 2 - Online video link extractor</source>
         <translation>SVPtube 2</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="196"/>
+        <location filename="tubeplugin.cpp" line="205"/>
         <source>Choose video player</source>
         <translation>Выберите проигрыватель видео</translation>
     </message>
     <message>
-        <location filename="tubeplugin.cpp" line="218"/>
+        <location filename="tubeplugin.cpp" line="231"/>
         <source>Choose 3D video player</source>
         <translation>Выберите проигрыватель видео для 3D</translation>
     </message>
